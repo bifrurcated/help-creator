2b5cb82d947fa3af1825f4de3743291fbbff696c
bifurcated <litvinenko-maks@inbox.ru>
2022-06-18 00:02:41 +1000

    Add method for copy and delete all files and directory

--- src/main/java/ru/vvsu/helpcreator/utils/FileHelper.java
+++ src/main/java/ru/vvsu/helpcreator/utils/FileHelper.java
@@ -1,6 +1,12 @@
 package ru.vvsu.helpcreator.utils;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import static ru.vvsu.helpcreator.utils.ProjectSettings.*;
 
 public class FileHelper {
     public static void serialize(Serializable object, String path) {
@@ -26,5 +32,46 @@ public class FileHelper {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public static void copyDirectory(String sourceDirectoryLocation, String destinationDirectoryLocation) throws IOException {
+        Files.walk(Paths.get(sourceDirectoryLocation))
+                .filter(source -> !source.getFileName().toString().equals(MAIN_PAGE_NAME+HTML_SUFFIX))
+                .filter(source -> !source.getFileName().toString().equals(PAGE_NAME+HTML_SUFFIX))
+                .forEach(source -> {
+                    Path destination = Paths.get(destinationDirectoryLocation, source.toString()
+                            .substring(sourceDirectoryLocation.length()));
+                    if (!(Files.exists(destination) && Files.isDirectory(destination))) {
+                        try {
+                            Files.copy(source, destination, StandardCopyOption.REPLACE_EXISTING);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+    }
+
+    public static void deleteDirectory(File directory) {
+
+        // if the file is directory or not
+        if(directory.isDirectory()) {
+            File[] files = directory.listFiles();
+
+            // if the directory contains any file
+            if(files != null) {
+                for(File file : files) {
+
+                    // recursive call if the subdirectory is non-empty
+                    deleteDirectory(file);
+                }
+            }
+        }
+
+        if(directory.delete()) {
+            System.out.println(directory + " is deleted");
+        }
+        else {
+            System.out.println("Directory not deleted");
+        }
     }
 }
