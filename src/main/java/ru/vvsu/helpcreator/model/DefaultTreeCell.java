61b5df9b4a1b5e3c50bf46ce5ed92a7a4ca828b8
bifurcated <litvinenko-maks@inbox.ru>
2022-06-20 05:06:34 +1000

    Replace to with pattern variable

--- src/main/java/ru/vvsu/helpcreator/model/DefaultTreeCell.java
+++ src/main/java/ru/vvsu/helpcreator/model/DefaultTreeCell.java
@@ -8,8 +8,7 @@ public class DefaultTreeCell<T> extends TextFieldTreeCell<T> {
     @Override
     public void updateItem(T item, boolean empty) {
         super.updateItem(item, empty);
-        if (!empty && item instanceof Page) {
-            Page page = (Page) item;
+        if (!empty && item instanceof Page page) {
             setText(page.getName());
             FontIcon icon = new FontIcon("anto-file-text");
             icon.setIconSize(8);
@@ -17,10 +16,9 @@ public class DefaultTreeCell<T> extends TextFieldTreeCell<T> {
         } else if (empty) {
             setText(null);
             setGraphic(null);
-        } else if (item instanceof Node) {
+        } else if (item instanceof Node newNode) {
             setText(null);
             Node currentNode = getGraphic();
-            Node newNode = (Node) item;
             if (currentNode == null || !currentNode.equals(newNode)) {
                 setGraphic(newNode);
             }
