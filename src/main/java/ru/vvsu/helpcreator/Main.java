0ad9fc5a327f7313e285a1e5ad223417fe29c9c6
bifurcated <litvinenko-maks@inbox.ru>
2022-06-19 16:07:35 +1000

    Remove preferences clear

--- src/main/java/ru/vvsu/helpcreator/Main.java
+++ src/main/java/ru/vvsu/helpcreator/Main.java
@@ -3,12 +3,9 @@ package ru.vvsu.helpcreator;
 import javafx.application.Application;
 import javafx.application.HostServices;
 import javafx.stage.Stage;
-import ru.vvsu.helpcreator.model.Project;
 import ru.vvsu.helpcreator.utils.ViewWindow;
 
 import java.io.IOException;
-import java.util.prefs.BackingStoreException;
-import java.util.prefs.Preferences;
 
 public class Main extends Application {
 
@@ -21,12 +18,6 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
-        Preferences preferences = Preferences.userNodeForPackage(Project.class);
-        try {
-            preferences.clear();
-        } catch (BackingStoreException e) {
-            e.printStackTrace();
-        }
         launch();
     }
 }
